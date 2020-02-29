d0648a3fefb61d7ff7869ec7195ffefd1f8356ca
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2020-02-29 10:07:44 +0300

    Create static gamefield

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -1,15 +1,51 @@
 import React from 'react';
 import styles from './styles.css'
+import Gapped from '@skbkontur/react-ui/Gapped';
+import PropTypes from 'prop-types';
+
+export const Row = () => {
+    return (<div>
+        <Gapped gap={3}>
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+            <Cell class={styles.wall} />
+        </Gapped>
+    </div>)
+};
+
+export const Cell = (props) => {
+    return (<div className={styles.cell}>
+        <img className={props.class}/>
+    </div>);
+};
+
+Cell.propTypes = {
+    class: PropTypes.string
+};
 
 export default class Field extends React.Component {
-    render () {
-        return (
-            <div className={ styles.root }>
-                <script>
-                    {window.addEventListener("keydown", this.handlerClick)};
-                </script>
-            </div>
-        );
+    render() {
+        return (<div className={styles.root}>
+            <Gapped vertical gap={3}>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+                <Row/>
+            </Gapped>
+        </div>);
     }
 
     handlerClick (event) {
@@ -31,3 +67,4 @@ export default class Field extends React.Component {
         }
     }
 }
+
