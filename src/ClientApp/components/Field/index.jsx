550f805841e8c9d6b697d00e081d98a57bda9116
Jaggernaut09 <mrhomka.com@gmail.com>
2020-02-29 14:36:52 +0500

    add key down handler

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -4,7 +4,30 @@ import styles from './styles.css'
 export default class Field extends React.Component {
     render () {
         return (
-            <div className={ styles.root }/>
+            <div className={ styles.root }>
+                <script>
+                    {window.addEventListener("keydown", this.handlerClick)};
+                </script>
+            </div>
         );
+    }
+
+    handlerClick (event) {
+        switch (event.key) {
+            case "ArrowUp":
+                console.log("up");
+                break;
+            case "ArrowDown":
+                console.log("down");
+                break;
+            case "ArrowLeft":
+                console.log("left");
+                break;
+            case "ArrowRight":
+                console.log("right");
+                break;
+            default:
+                return
+        }
     }
 }
