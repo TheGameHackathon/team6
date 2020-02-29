a09a29283b8b846bbdc48611ad5ba33f0b27884b
Kamiikoto <38423206+Kamikoto-sama@users.noreply.github.com>
2020-02-29 16:08:22 +0500

    GameFieldsProvider

---
+++ src/Models/FieldCell.cs
@@ -0,0 +1,14 @@
+﻿namespace thegame.Models
+{
+	public class FieldCell
+	{
+		public FieldCell(BackgroundObject type, ForegroundObject obj)
+		{
+			Type = type;
+			Object = obj;
+		}
+
+		public BackgroundObject Type { get; set; }
+		public ForegroundObject Object { get; set; }
+	}
+}
---
+++ src/Models/GameField.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace thegame.Models
+{
+	public class GameField
+	{
+		public FieldCell[][] Cells { get; set; }
+		public Point PlayerPosition { get; set; }
+	}
+}
