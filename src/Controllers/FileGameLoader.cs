70503b8e6f3897bc93fee4a29771c96af4d7ae11
Fezoo <dtopcode@gmail.com>
2020-03-13 15:45:17 +0300

    Добавил методы по загрузке файлов по номеру уровня.

--- src/Controllers/FileGameLoader.cs
+++ src/Controllers/FileGameLoader.cs
@@ -5,8 +5,14 @@ namespace thegame.Controllers
 {
     class FileGameLoader
     {
-        public static string[] Load(string filename)
+        private const string GAME_DATA_PATH = @"GameData";
+
+        private static string GameDataPathFormat => GAME_DATA_PATH + @"\{0}.txt";
+
+        public static string[] Load(int level)
         {
+            var filename = GetFileNameByLevel(level);
+
             if (string.IsNullOrEmpty(filename))
                 throw new ArgumentException();
 
@@ -14,6 +20,16 @@ namespace thegame.Controllers
                 throw new FileNotFoundException();
 
             return File.ReadAllLines(filename);
+        }
+
+        public static int GetLevelsAmount()
+        {
+            return Directory.GetFiles(string.Format(GAME_DATA_PATH)).Length;
+        }
+
+        private static string GetFileNameByLevel(int level)
+        {
+            return string.Format(GameDataPathFormat, level);
         }
     }
 }
--- src/Controllers/GamesController.cs
+++ src/Controllers/GamesController.cs
@@ -10,15 +10,22 @@ namespace thegame.Controllers
     public class GamesController : Controller
     {
         [HttpPost]
-        public IActionResult Index()
+        public IActionResult Index(int level = 1)
         {
-            var stringCells = FileGameLoader.Load("GameData\\1.txt");
+            var stringCells = FileGameLoader.Load(level);
             var cells = stringCells.ParsingCells();
 
             Game game = new Game(cells);
             GamesRepo.AddGame(game);
 
             return new ObjectResult(game.GameField);
+        }
+
+        [Route("levels")]
+        [HttpGet]
+        public IActionResult Levels()
+        {
+            return new ObjectResult(FileGameLoader.GetLevelsAmount());
         }
     }
 }
