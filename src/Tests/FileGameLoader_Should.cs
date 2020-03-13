e6471ce9e9acf7d11564bbbbd9d95bd668c2ca1c
Fezoo <dtopcode@gmail.com>
2020-03-13 12:36:09 +0300

    Добавил фреймворк для тестов.

---
+++ src/Tests/FileGameLoader_Should.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace thegame.Tests
+{
+    [TestFixture]
+    class FileGameLoader_Should
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+    }
+}
