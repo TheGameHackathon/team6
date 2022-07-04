6ce0a7b3b451763899d072754015f4a248414049
DEFLIK <klimmail1@yandex.ru>
2022-07-04 16:08:12 +0500

    view controller

---
+++ src/Controllers/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using thegame.Models;
+using thegame.Models.Dto;
+using thegame.Services;
+
+namespace thegame.Controllers;
+
+[Route("api/view")]
+public class ViewController : Controller
+{
+    private GamesService games;
+
+    public ViewController(GamesService games)
+    {
+        this.games = games;
+    }
+
+    [HttpGet("/view/{gameId}")]
+    [Produces("application/json")]
+    public IActionResult View([FromRoute] Guid gameId)
+    {
+        var game = games.GetOrNull(gameId);
+
+        if (game == null)
+        {
+            NotFound();
+        }
+
+        return Ok(game.AGameDto());
+
+    }
+}
